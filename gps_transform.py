299d6cf973223d785b31c23b2b2ee4c86e0d6342
Bill Williams <divebilldive@gmail.com>
2021-09-20 17:12:18 -0700

    moved distance_optimzer to separate file

--- gps_transform.py
+++ gps_transform.py
@@ -1,3 +1,6 @@
+from geopy import distance
+import pandas as pd
+
 #
 # precision optimizer - drop 2 digits from lat, lon, and ele.
 #
@@ -6,3 +9,49 @@ def precision_optimizer(points_list, precision_reduction):
        gps_point['latitude'] = gps_point['latitude'][0:len(gps_point['latitude']) - precision_reduction]
        gps_point['longitude'] = gps_point['longitude'][0:len(gps_point['longitude']) - precision_reduction]
        gps_point['elevation'] = gps_point['elevation'][0:len(gps_point['elevation']) - precision_reduction]
+
+
+def distance_optimizer(points_list, minimum_distance):
+    gps_distance = list()
+    count = 0
+    points_stripped = 0
+
+    for index, gps_point in enumerate(points_list):
+        
+        current_point = (gps_point['latitude'], gps_point['longitude'])
+
+        while True:
+            if index == len(points_list) - 1:
+                # break loop when last element detected
+                #print(count)
+                break
+
+            next_point = (points_list[index+1]['latitude'], points_list[index+1]['longitude'])
+            mydistance = distance.distance(current_point, next_point)
+        
+            if round(mydistance.m,1) < minimum_distance:
+                points_list.pop(index+1)
+                points_stripped += 1
+                continue
+            else:
+                gps_distance.append(round(mydistance.m, 1))    
+                count += 1
+                #break the while
+                break
+        #
+        # debug code prints lat/lon and delta for points
+        #
+        #if index == 0:
+        #    print("\npoint 0 lat lon : %s %s\npoint 1 lat lon : %s %s\ndistance (m) : %f\n" % 
+        #            (gps_point['latitude'], gps_point['longitude'], dataList[index+1]['latitude'], dataList[index+1]['longitude'], round(mydistance.m, 1)))
+
+
+
+    print('GPS points stripped @ %dm\t: %d' % (minimum_distance, points_stripped))  
+    print('GPS points after distance strip\t: %d' % (len(points_list)))
+    #percent_reduction = round(100 * (points_stripped / imported_points), 1)
+
+   # print('GPS points reduced %g percent' % (percent_reduction))  
+    points_df = pd.DataFrame(gps_distance, index=None, columns=['Distance'])
+    print('\nMean\t: %f\nMedian\t: %f\nMin\t: %f\nMax\t: %f' % (points_df.mean(), points_df.median(), points_df.min(), points_df.max()))
+
--- gpx_re.py
+++ gpx_re.py
@@ -2,10 +2,10 @@
 # imports
 #
 import numpy as np
-import pandas as pd
+#import pandas as pd
 import re as re
 from jinja2 import Environment, FileSystemLoader
-from geopy import distance
+
 import gps_transform
 
 
@@ -78,51 +78,10 @@ print('\nGPS points imported\t\t: %d' % imported_points)
 
 
 
-gps_distance = list()
-count = 0
-points_stripped = 0
-for index, gps_point in enumerate(dataList):
-    
-    current_point = (gps_point['latitude'], gps_point['longitude'])
-
-    while True:
-        if index == len(dataList) - 1:
-            # break loop when last element detected
-            #print(count)
-            break
-
-        next_point = (dataList[index+1]['latitude'], dataList[index+1]['longitude'])
-        mydistance = distance.distance(current_point, next_point)
-    
-        if round(mydistance.m,1) < GPS_POINT_DELTA:
-            dataList.pop(index+1)
-            points_stripped += 1
-            continue
-        else:
-            gps_distance.append(round(mydistance.m, 1))    
-            count += 1
-            #break the while
-            break
-    #
-    # debug code prints lat/lon and delta for points
-    #
-    #if index == 0:
-    #    print("\npoint 0 lat lon : %s %s\npoint 1 lat lon : %s %s\ndistance (m) : %f\n" % 
-    #            (gps_point['latitude'], gps_point['longitude'], dataList[index+1]['latitude'], dataList[index+1]['longitude'], round(mydistance.m, 1)))
-
-
-
-print('GPS points stripped @ %dm\t: %d' % (GPS_POINT_DELTA, points_stripped))  
-print('GPS points after distance strip\t: %d' % (len(dataList)))
-percent_reduction = round(100 * (points_stripped / imported_points), 1)
-
-print('GPS points reduced %g percent' % (percent_reduction))  
-points_df = pd.DataFrame(gps_distance, index=None, columns=['Distance'])
-print('\nMean\t: %f\nMedian\t: %f\nMin\t: %f\nMax\t: %f' % (points_df.mean(), points_df.median(), points_df.min(), points_df.max()))
-
-
-
-
+#
+# GPS Transforms
+#
+gps_transform.distance_optimizer(dataList, GPS_POINT_DELTA)
 gps_transform.precision_optimizer(dataList, 2)
 
 #
