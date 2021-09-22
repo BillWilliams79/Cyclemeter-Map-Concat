6c962237a0ea1256692044de8cdc4045bcdf2a8b
Bill Williams <divebilldive@gmail.com>
2021-09-21 20:11:24 -0700

    rest of initial flask integration. Have to stop fooling around with git add vs commit -a

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from werkzeug.utils import redirect
+import gps_transform, gps_extract, gps_load
 
 app = Flask(__name__)
 
-@app.route('/')
+gpx_etl_op = dict()
+gps_points = list()
+gpx_etl_op["gps_points"] = gps_points
+#
+# initilize counters to zero
+#
+gpx_etl_op["imported_points"] = 0
+gpx_etl_op["delta_points_stripped"] = 0
+gpx_etl_op["current_points"] = 0
+
+
+@app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/transferload", methods=["POST"])
+def transferload():
+    if request.method == 'POST':
+        file_name = request.form.get("fname")
+        min_delta = request.form.get("mdist")
+        gpx_etl_op["extract_file_name"] = file_name
+        gpx_etl_op["gps_min_delta"] = min_delta
+        gpx_etl_op["gps_precision_reduction"] = 2
+        gps_extract.gpx_file_extract(gpx_etl_op)
+        return render_template('transferload.html', etl_op=gpx_etl_op)
+    else:
+        redirect("/")    
 if __name__ == "__main__":
     app.run(debug=True)
