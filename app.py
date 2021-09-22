a2cb80b67220c61fde28a34db56f851be5c6e5e7
Bill Williams <divebilldive@gmail.com>
2021-09-22 01:32:54 -0700

    added ability to set precision optimizer to values 0,1,2

--- app.py
+++ app.py
@@ -31,9 +31,10 @@ def gps_transform_web():
     if request.method == 'POST':
         file_name = request.form.get("file_name")
         min_delta = int(request.form.get("min_dist"))
+        precision_reduction = int(request.form.get("precision_optimizer"))
         gpx_etl_op["extract_file_name"] = file_name
         gpx_etl_op["gps_min_delta"] = min_delta
-        gpx_etl_op["gps_precision_reduction"] = 2
+        gpx_etl_op["gps_precision_reduction"] = precision_reduction
         gps_extract.gpx_file_extract(gpx_etl_op)
         return render_template('gps_transform.html', etl_op=gpx_etl_op)
     else:
@@ -58,7 +59,7 @@ def gps_complete_web():
         #
         # GPS Transform
         #
-        gpx_etl_op["load_file_name"] = 'Strava GPX {gps_min_delta}m delta'.format(**gpx_etl_op)
+        gpx_etl_op["load_file_name"] = 'Strava GPX {gps_min_delta}m {gps_precision_reduction}pr delta'.format(**gpx_etl_op)
         gps_load.gpx_file_load(gpx_etl_op)
         return render_template('gps_complete.html', etl_op=gpx_etl_op)
     else:
